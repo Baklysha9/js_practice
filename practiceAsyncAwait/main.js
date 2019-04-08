388d95c7cbe6c5129dfa250689f2fd5e1ee07aaf
Baklysha9 <bogdandynay@gmail.com>
2019-04-08 08:39:59 +0300

    add film card

--- practiceAsyncAwait/main.js
+++ practiceAsyncAwait/main.js
@@ -67,23 +67,55 @@ async function load() {
 
 document.body.addEventListener('click', (e) => {
     let target = e.target;
-    console.log(target);
+    //console.log(target);
     if (target.className == "borderBox") {
-      if (target.className != "fa fa-star icon" || target.className != "fa fa-star-o icon") {
-        let film_name = target.querySelector('.text h2 span').innerText;
-        getMovie('http://www.omdbapi.com/?t=' + film_name.toLowerCase() + '&apikey=d5677312');
-      }
-    }
-  });
+        if (target.className != "fa fa-star icon" || target.className != "fa fa-star-o icon") {
+            let film_name = target.querySelector('.borderBox h2').innerHTML;
+            //console.log(film_name);
+            loadFilmPage(film_name);
+        }
+    } else if (target.id == "image" || target.id == "name" || target.id == "released") {
+        let film_name = target.parentNode.querySelector('.borderBox h2').innerHTML;
+        loadFilmPage(film_name); 
+    } else {
+        // document.getElementById('filmPage').style.display= 'none';
+        // document.querySelector('#main').style.opacity = 1;
 
-const getMovie = async (url) => {
+    }
+});
+
+const loadFilmPage = async (name) => {
+    console.log(name);
+    let film = {};
+    let url = 'http://www.omdbapi.com/?t=' + name + '&apikey=d5677312';
     try {
         let response = await fetch(url);
-        let data = await response.json();
-        console.log(data);
-    }catch (e) {
+        film = await response.json();
+    } catch (e) {
         console.log('ERROR =', e);
     }
+    document.querySelector('#main').style.opacity = 0.2;
+    document.querySelector('#main').style.pointerEvents = 'none';
+    document.getElementById('filmPage').classList.add('filmPage');
+    document.getElementById('filmPage').style.display ='block';
+    document.getElementById('filmPage').innerHTML = '';
+    let img = document.createElement('img');
+    let h1 = document.createElement('h1');
+    let  = document.createElement('h1');
+    console.log(film);
+    img.src = film.Poster;
+    img.style.width = '400px';
+
+    document.getElementById('filmPage').appendChild(img);
+    document.getElementById('filmPage').innerHTML += "<div style='display:inline-block;vertical-align:top;width: 50%;text-align: center; margin-top:50px;'><h1 style=''>"+film.Title+"</h1>"+"<h3>"+film.Released+"</h3>"+film.Plot+"</h3>"+"<h1 class='buttonClose'>x</h1>"+"<h3 style='margin-top:50px;'>Actors:<br>"+film.Actors+"</h3>"+"<h3 style='margin-top:50px;'>Imdb rating:<br>"+film.imdbRating+"</h3>"+"<a href ='https://www.imdb.com/title/"+film.imdbID+"'><img style='width:150px; height:40px;' src='IMDB_Logo_2016.png'></a>"+"</div>";
+
+    document.querySelector('.buttonClose').addEventListener('click', function () {
+        document.getElementById('filmPage').style.display ='none';
+        document.querySelector('#main').style.opacity = 1;
+        document.querySelector('#main').style.pointerEvents = '';
+    });
+
+
 }
 
 document.getElementById('resultButton').addEventListener('click', function () {
